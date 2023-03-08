e5c72af2e45ba04b1e5d967a51f1bb4d591269cc
arvindpratap69 <satyamsinghp40@gmail.com>
2023-03-08 16:24:25 +0530

    upload

---
+++ login/app/Http/Livewire/ShopComponent.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Livewire;
+
+// use livewire;
+// use Gloudemans\Shoppingcart\Cart;
+use Cart;
+use App\Models\Product;
+use Livewire\Component;
+use App\Models\Category;
+use Livewire\WithPagination;
+
+
+class ShopComponent extends Component
+{
+    public $sorting;
+    public $pagesize;
+
+    public function mount()
+    {
+        $this->sorting = "default";
+        $this->pagesize = 12;
+    }
+    public function store($product_id,$product_name,$product_price)
+    {
+        Cart::add($product_id,$product_name,1,$product_price)->associate('App\Models\Product');
+        session()->flash('success_message','Item added in Cart');
+        return  redirect()->route('product.cart');
+    }
+    use WithPagination;
+    public function render()
+    {
+        if($this->sorting=='date')
+        {
+            $products = Product::orderBy('created_at','DESC')->paginate($this->pagesize);
+        }
+        elseif($this->sorting=="price")
+        {
+            $products = Product::orderBy('regular_price','ASC')->paginate($this->pagesize);
+
+        }
+        elseif($this->sorting=="price-desc")
+        {
+            $products = Product::orderBy('regular_price','DESC')->paginate($this->pagesize);
+
+        }
+        else
+        {
+            $products = Product::paginate($this->pagesize);
+
+        }
+        $categories = Category::all();
+        // $products = Product::paginate(12);
+        return view('livewire.shop-component',['products'=>$products,'categories'=>$categories])->layout('layouts.base');
+    }
+}
